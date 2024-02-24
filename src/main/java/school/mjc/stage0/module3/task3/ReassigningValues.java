c6a1ed19a3eea9fd5ff275c59a7fd16d03eb7da0
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-25 00:38:33 +0300

    module 3. task 3. fix

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -5,9 +5,11 @@ public class ReassigningValues {
         int first = 1;
         int second = 10;
         int third = 100;
+
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
+
         int linkToFirst = first;
         int linkToSecond = second;
         int linkToThird = third;
